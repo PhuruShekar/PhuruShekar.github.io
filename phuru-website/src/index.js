ece0381765b194fcd3a239be8d2e1a2cbf5b8aa1
Phuru Shekar <phurushotham@gmail.com>
2018-07-16 00:30:36 -0400

    Revert "Merge pull request #1 from PhuruShekar/site-making-v2"
    This reverts commit 725b726276d10c74d9ad07412c6bacce624177cc, reversing
    changes made to 1559f8c60fe2e100676c9a6d3a328f40923e03de.

--- phuru-website/src/index.js
+++ phuru-website/src/index.js
@@ -1,8 +1,6 @@
-//belonged in package.json as test
-////"test": "react-scripts test --env=jsdom",
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
