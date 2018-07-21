c324124de945aa6e522ac8c2d4eb540728aa530c
Phuru Shekar <phurushotham@gmail.com>
2018-07-20 20:07:03 -0400

    added bg, added my img to home downloaded some useless packages

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import {ProgressBar} from 'react-bootstrap/lib';
+import me from '../../Assets/img/me.jpg';
 
 class HomePage extends Component {
   render() {
     return (
-        <div className="container-fluid">
-            <main>
+        <div className="overlay">
+            <main className="container-fluid">
 
                 <h1>
-                    Hey! Welcome to the site!
+                    Welcome!
                 </h1>
+                <img src={me} className="me" />
 
                 <p>
-                    Don't rush me, I'm working on the site as fast as I can!
+                Don't rush me, I'm working on the site as fast as I can!
                 </p>
-
+    
                 <ProgressBar active bsStyle="danger" now={"15"} label={"15%"} />
+
             </main>
+
         </div>
     );
   }
 }
+
 export default HomePage;
