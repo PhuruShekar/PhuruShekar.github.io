7bdda9b6f0d9849fe91a2be8099c59d44f11a28e
Phuru <phurushotham@gmail.com>
2019-09-01 01:25:27 -0400

    new logo :)

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
 import doc from "../../Assets/doc/Phuru_Shekar_Resume.pdf";
+import logo from "../../Assets/img/logo.png";
 
 class Header extends Component {
   render() {
@@ -9,7 +10,7 @@ class Header extends Component {
       <Navbar className="nav"  fixedTop>
         <Navbar.Header>
         <Navbar.Brand>
-          <Link to="/">Phuru</Link>
+          <Link to="/"><img src={logo} height="150%" width="auto" /></Link>
         </Navbar.Brand>
         <Navbar.Toggle />
         </Navbar.Header>
